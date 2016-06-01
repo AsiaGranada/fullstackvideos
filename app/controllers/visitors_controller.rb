e91773e420767ed989537d01f80946119fa9be5c
Daniel Kehoe <daniel@danielkehoe.com>
2016-06-01 20:07:00 +1000

    show list of videos on welcome page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
   def index
     @resource = User.new
     @resource.coupon = Coupon.new
+    @freevideos = Video.all
+    @paidvideos = Video.all
   end
 
 end
