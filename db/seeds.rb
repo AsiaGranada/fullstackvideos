70edabc2677b1563a9ebbfe171ba8fa68754e81d
Daniel Kehoe <daniel@danielkehoe.com>
2016-06-03 13:08:04 +1000

    fix db seeds sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,6 @@ user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 CreateCouponcodesService.new.call
 puts 'CREATED PROMOTIONAL CODES'
-video1 = Video.create([{ title: 'Find Rubygems' }, { description: 'Learn to find and evaluate Rubygems.' }, { wistia: 'pc2jpdwzob' }])
-video2 = Video.create([{ title: 'What Are Rubygems?' }, { description: 'Learn how to add features in your website using Rubygems.' }, { wistia: '5ixtput3r7' }])
+video1 = Video.find_or_create_by!(title: 'Find Rubygems', level: 'beginner', description: 'Learn to find and evaluate Rubygems.', wistia: 'pc2jpdwzob')
+video2 = Video.find_or_create_by!(title: 'What Are Rubygems?', level: 'free',  description: 'Learn how to add features in your website using Rubygems.', wistia: '5ixtput3r7')
+video3 = Video.find_or_create_by!(title: 'XCode Command Line Tools', level: 'advanced',  description: 'Installing XCode Command Line Tools.', wistia: '5ixtput3r7')
