f353acd269ee35bb5e309d8ced5f8f9164f68bb8
Daniel Kehoe <daniel@danielkehoe.com>
2016-06-14 12:42:08 +1000

    welcome page gets newest video automatically

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,7 @@ class VisitorsController < ApplicationController
   def index
     @resource = User.new
     @resource.coupon = Coupon.new
+    @thisweekvideo = Video.all.order('created_at DESC').first
     @freevideos = Video.free.order('created_at DESC')
     @advancedvideos = Video.advanced.order('created_at DESC')
     @beginnervideos = Video.beginner.order('created_at DESC')
