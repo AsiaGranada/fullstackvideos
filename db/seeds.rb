6d463cc50ed3a18ca58324c643273f108f5e31f3
Daniel Kehoe <daniel@danielkehoe.com>
2016-06-16 15:19:34 +1000

    create coupons before users in seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
 CreateCouponcodesService.new.call
 puts 'CREATED PROMOTIONAL CODES'
+user = CreateAdminService.new.call
+puts 'CREATED ADMIN USER: ' << user.email
 video1 = Video.find_or_create_by!(title: 'Find Rubygems', level: 'beginner', description: 'Learn to find and evaluate Rubygems.', wistia: 'pc2jpdwzob')
 video2 = Video.find_or_create_by!(title: 'What Are Rubygems?', level: 'free',  description: 'Learn how to add features in your website using Rubygems.', wistia: '5ixtput3r7')
 video3 = Video.find_or_create_by!(title: 'XCode Command Line Tools', level: 'advanced',  description: 'Installing XCode Command Line Tools.', wistia: '5ixtput3r7')
