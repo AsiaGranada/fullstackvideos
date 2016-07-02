206baa94c6857557913d1534a52aabd22916ed5b
Daniel Kehoe <daniel@danielkehoe.com>
2016-07-02 15:15:41 +0800

    add page for free subscription

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :coupons
   post 'subscriptions/expire', to: 'subscriptions#expire'
   get 'products/:id', to: 'products#show', :as => :products
+  get 'invite', to: 'high_voltage/pages#show', id: 'invitations'
   devise_for :users, :controllers => { :registrations => 'registrations' }
   resources :users
   mount StripeEvent::Engine, at: '/stripe'
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@ CreateCouponcodesService.new.call
 puts 'CREATED PROMOTIONAL CODES'
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
-video1 = Video.find_or_create_by!(title: 'Find Rubygems', level: 'beginner', description: 'Learn to find and evaluate Rubygems.', wistia: 'pc2jpdwzob')
-video2 = Video.find_or_create_by!(title: 'What Are Rubygems?', level: 'free',  description: 'Learn how to add features in your website using Rubygems.', wistia: '5ixtput3r7')
-video3 = Video.find_or_create_by!(title: 'XCode Command Line Tools', level: 'advanced',  description: 'Installing XCode Command Line Tools.', wistia: '5ixtput3r7')
+video1 = Video.find_or_create_by!(level: 'advanced', wistia: 'ypjjllo02h')
+video2 = Video.find_or_create_by!(level: 'free', wistia: 'lykvsxec8j')
+video3 = Video.find_or_create_by!(level: 'beginner', wistia: 'zhrplosxxw')
+video4 = Video.find_or_create_by!(level: 'beginner', wistia: '9q4w7nvzai')
+video5 = Video.find_or_create_by!(level: 'free', wistia: 'cavh41izma')
