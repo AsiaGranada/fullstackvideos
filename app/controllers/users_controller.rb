0c8dc512b51d651c17a93e226922b1dcf0f99378
Daniel Kehoe <daniel@danielkehoe.com>
2016-07-07 16:01:18 +0800

    add paigination for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :admin_only, :except => :show
 
   def index
-    @users = User.all
+    @users = User.order(created_at: :desc).page(params[:page]).per(20)
   end
 
   def show
